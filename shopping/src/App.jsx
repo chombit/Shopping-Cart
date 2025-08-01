db4f5ec7e1e9d2961e7cab20dcbce9d6c74bd7e0
chombit <114684503+chombit@users.noreply.github.com>
2025-08-01 17:03:58 +0300

    package

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -1,10 +1,12 @@
-
+import Card from './components/Card'
 import './App.css'
 
 function App() {
   return (
     <>
       <h1>Shopping Cart</h1>
+      
+      <Card />
     </>
   )
 }
---
+++ shopping/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+   
+<div>
+    <header class="bg-blue-600 text-white p-4">
+        <h1 class="text-2xl">My Website</h1>
+    </header>
+    <main class="p-6">
+        <h2 class="text-xl font-semibold">Welcome to My Website</h2>
+        <p>This is a simple layout using Tailwind CSS.</p>
+    </main>
+
+    <footer class="bg-gray-800 text-white text-center p-4">
+        <p>&copy; 2023 My Website</p>
+    </footer>
+     </div>
+
+
+  )
+}
